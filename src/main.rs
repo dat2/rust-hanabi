8549e2cb7b17f15b99c22139c9c8c8e3e7f83cc2
Nicholas Dujay <ndujay05@ca.ibm.com>
2016-08-02 20:55:51 -0400

    implemented @dashed's suggestions, figured out to_json

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,23 @@ extern crate serde;
 extern crate serde_json;
 extern crate ws;
 extern crate dotenv;
+extern crate rand;
 
 // import local
 mod messages;
 
 use dotenv::dotenv;
 use std::rc::Rc;
-use std::cell::{Cell,RefCell};
+use std::cell::RefCell;
 use std::collections::HashMap;
 use ws::{listen,Message,Sender};
-use serde_json::{Value, Map};
+use serde_json::{Value, Map, to_value};
 use messages::{JsonMessage,Event};
+use rand::{thread_rng, Rng};
 
-#[derive(Debug)]
-enum Colour {
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+enum Colour
+{
   White,
   Yellow,
   Green,
@@ -28,24 +31,24 @@ enum Colour {
   Blue
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct Card
 {
   colour: Colour,
   number: u64
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct GameState
 {
-  deck: RefCell<Vec<Card>>
+  deck: Vec<Card>
 }
 
 impl GameState
 {
   fn new(max_num: u64) -> GameState
   {
-    let mut g = GameState { deck: RefCell::new(Vec::new()) };
+    let mut g = GameState { deck: Vec::new() };
     g.gen_deck(max_num);
     g.shuffle_deck();
     g
@@ -54,18 +57,19 @@ impl GameState
   fn gen_deck(&mut self, max_num: u64)
   {
     for x in 1..max_num+1 {
-      self.deck.borrow_mut().push(Card { colour: Colour::White, number: x });
-      self.deck.borrow_mut().push(Card { colour: Colour::Yellow, number: x });
-      self.deck.borrow_mut().push(Card { colour: Colour::Green, number: x });
-      self.deck.borrow_mut().push(Card { colour: Colour::Red, number: x });
-      self.deck.borrow_mut().push(Card { colour: Colour::Blue, number: x });
+      self.deck.push(Card { colour: Colour::White, number: x });
+      self.deck.push(Card { colour: Colour::Yellow, number: x });
+      self.deck.push(Card { colour: Colour::Green, number: x });
+      self.deck.push(Card { colour: Colour::Red, number: x });
+      self.deck.push(Card { colour: Colour::Blue, number: x });
     }
   }
 
   // knuth shuffle
   fn shuffle_deck(&mut self)
   {
-
+    let mut rng = thread_rng();
+    rng.shuffle(&mut self.deck);
   }
 }
 
@@ -76,20 +80,17 @@ fn main()
 {
   dotenv().ok();
 
-  let game = GameState::new(5);
-  println!("{:?}", game);
-
   let registry: Registry = Rc::new(RefCell::new(HashMap::new()));
-  let id_counter = Cell::new(0);
+  let mut id_counter = 0;
 
   // the main server handler
   listen(dotenv!("BIND"), |out|
   {
-    id_counter.set(id_counter.get() + 1);
+    id_counter = id_counter + 1;
 
     Handler
     {
-      id: id_counter.get(),
+      id: id_counter,
       out: Rc::new(out),
       registry: registry.clone(),
     }
@@ -102,9 +103,8 @@ fn main()
 // return a string to send back to the client
 fn handle_init(_: &Value, out: Rc<Sender>) -> Result<(), ws::Error>
 {
-  let mut map = Map::new();
-  map.insert("test".to_string(), Value::I64(123));
-  let response = JsonMessage { event: Event::Init, data: Value::Object(map) };
+  let game = GameState::new(5);
+  let response = JsonMessage { event: Event::Init, data: to_value(&game) };
 
   // return the string to send
   let response = serde_json::to_string(&response).unwrap();
@@ -125,7 +125,6 @@ impl ws::Handler for Handler {
   fn on_open(&mut self, _: ws::Handshake) -> ws::Result<()>
   {
     self.registry.borrow_mut().insert(self.id, self.out.clone());
-    println!("Open connection: {:?}", self.id);
     Ok(())
   }
 
